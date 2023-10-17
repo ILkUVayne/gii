9c28b21aa817b58dbecd7726a37a27f8d66a9da6
luoyang <1193055746@qq.com>
2023-10-17 16:10:21 +0800

    add: add mysql driver

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"gii/demo/config"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func main() {
-	config.Router().Run("localhost:8000")
+	//config.Router().Run("localhost:8000")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/gii")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}(db)
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}(rows)
+
+	for rows.Next() {
+		var id int
+		var name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(id, name)
+	}
 }
